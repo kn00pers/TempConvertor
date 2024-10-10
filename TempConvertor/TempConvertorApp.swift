ee2ae61919282c5e15bcb6f52a950a98acf80e85
kn00pers <patplaypl@gmail.com>
2024-10-10 14:00:49 +0200

    Initial Commit

---
+++ TempConvertor/TempConvertorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempConvertorApp.swift
+//  TempConvertor
+//
+//  Created by Patryk Knopp on 10/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct TempConvertorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
